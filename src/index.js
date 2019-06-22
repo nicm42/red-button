e986d033a63bcf83f225668514787fe3d6028d12
Nic Mayer <nicmayer42@gmail.com>
2019-06-22 12:01:41 +0100

    Set up basic functionality

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+//import App from './App';
 
 
 class RedButton extends React.Component {
+  HandleClick(){
+  	alert("Please do not press this button again");
+  }
+
   render() {
-    return <button>&nbsp;</button>;
+    return (
+    	<div className="app">
+    		<div className="instructions">Press the red button.... if you dare</div>
+    		<button onClick={this.HandleClick}>&nbsp;</button>
+    	</div>
+    )
   }
 }
 
